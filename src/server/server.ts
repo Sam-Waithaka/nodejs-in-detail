f5f909abb9b4e1a7607a83746fb4ad1783795d8d
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-18 16:54:09 +0300

    Using the handlebars package for SSR

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,9 @@ import express, {Express } from "express";
 import { testHandler } from "./testHandler";
 import httpProxy from "http-proxy";
 import helmet from "helmet";
-import { registerCustomTemplateEngine } from "./custom_engine";
+//import { registerCustomTemplateEngine } from "./custom_engine";
+import { engine } from "express-handlebars";
+import * as helpers from "./template_helpers";
 
 
 const port = 5000;
@@ -12,20 +14,28 @@ const proxy = httpProxy.createProxyServer({
     target: "http://localhost:5100", ws: true
 });
 
-registerCustomTemplateEngine(expressApp)
+// registerCustomTemplateEngine(expressApp)
 expressApp.set('views', 'templates/server')
+expressApp.engine("handlebars", engine());
+expressApp.set("view engine", "handlebars")
 
 expressApp.use(helmet());
 expressApp.use(express.json());
 
+
 expressApp.get('/dynamic/:file', (req, resp) =>{
-    resp.render(
-        `${req.params.file}.custom`, 
-        {
-            message: 'Hello template',
-            req
-        },
-    )
+    // resp.render(
+    //     `${req.params.file}.custom`, 
+    //     {
+    //         message: 'Hello template',
+    //         req
+    //     },
+    // )
+
+    resp.render(`${req.params.file}.handlebars`,
+        { message: "Hello template", req,
+            helpers: { ...helpers }
+        });
 })
 expressApp.post("/test", testHandler);
 expressApp.use(express.static("static"));
