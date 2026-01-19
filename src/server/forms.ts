1453e7182b9cbd9ed4060a0f57a8ae8dc877f9e8
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-19 12:33:08 +0300

    sanitizing custom data using custom sanitizer

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,26 +1,37 @@
 import express, { Express } from "express";
 import multer from "multer";
+import { sanitizeValue } from "./sanitize";
+
+
 const fileMiddleware = multer({storage: multer.memoryStorage()});
+
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({extended: true}))
 }
+
 export const registerFormRoutes = (app: Express) => {
+
+
     app.get("/form", (req, resp) => {
         for (const key in req.query) {
             resp.write(`${key}: ${req.query[key]}\n`);
         }
         resp.end();
     });
+
+
     app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
-        resp.write(`Content-Type: ${req.headers["content-type"]}\n`)
-        for (const key in req.body) {
-            resp.write(`${key}: ${req.body[key]}\n`);
+        resp.setHeader('Content-Type', 'text/html')
+
+        for (const key in req.body){
+            resp.write(`<div>${key}: ${sanitizeValue(req.body[key])}</div>`)
         }
+
          if (req.file) {
-            resp.write(`---\nFile: ${req.file.originalname}\n`);
-            resp.write(req.file.buffer.toString());           
+            resp.write(`<div>File: ${req.file.originalname}</div>`);
+            resp.write(`<div>${sanitizeValue(req.file.buffer.toString())}</div>`);           
         }
-       
-        resp.end();
+
+        resp.end()
     });
 }
---
+++ src/server/sanitize.ts
@@ -0,0 +1,13 @@
+const matchPattern = /[&<>="'`]/g;
+const characterMappings: Record<string, string> = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    "\"": "&quot;",
+    "=": "&#x3D;", 
+    "'": "&#x27;",
+    "`": "&#x60;"
+};
+
+export const sanitizeValue = (value: string) =>
+    value?.replace(matchPattern, match => characterMappings[match])
