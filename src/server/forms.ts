054c774706a6972e5d9ea26f6a72547b70961351
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-19 11:21:25 +0300

    recieving form data with get requests

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -5,5 +5,13 @@ export const registerFormMiddleware = (app: Express) => {
 }
 
 export const registerFormRoutes = (app: Express) => {
-    
+    app.get('/form', (req, resp)=>{
+
+        // console.log('Request Object',req)
+
+        for (const key in req.query){
+            resp.write(`${key}: ${req.query[key]} \n`)
+        }
+        resp.end()
+    })
 }
