a5b6b5a053357c33fc311e4732d4bcdab78730f4
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-19 12:18:48 +0300

    Processing multipart data using multer js package

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,31 +1,26 @@
 import express, { Express } from "express";
-
+import multer from "multer";
+const fileMiddleware = multer({storage: multer.memoryStorage()});
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({extended: true}))
 }
-
 export const registerFormRoutes = (app: Express) => {
-    app.get('/form', (req, res)=>{
-
-        // console.log('Request Object',req)
-
-        for (const key in req.query){
-            res.write(`${key}: ${req.query[key]} \n`)
+    app.get("/form", (req, resp) => {
+        for (const key in req.query) {
+            resp.write(`${key}: ${req.query[key]}\n`);
         }
-        res.end()
-    })
-
-    app.post('/form', (req, res)=>{
-        res.write(`Content-Type: ${req.headers['content-type']} \n`)
-        
-        if (req.headers['content-type']?.startsWith('multipart/form-data')) {
-            req.pipe(res)
-        } else {
-            for (const key in req.body){
-            res.write(`${key}: ${req.body[key]}\n`)
+        resp.end();
+    });
+    app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
+        resp.write(`Content-Type: ${req.headers["content-type"]}\n`)
+        for (const key in req.body) {
+            resp.write(`${key}: ${req.body[key]}\n`);
         }
-        // req.pipe(res)
-        res.end()
-    }
-    })
+         if (req.file) {
+            resp.write(`---\nFile: ${req.file.originalname}\n`);
+            resp.write(req.file.buffer.toString());           
+        }
+       
+        resp.end();
+    });
 }
