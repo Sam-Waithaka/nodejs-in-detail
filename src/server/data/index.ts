3345171968ca7e8ca0fe9e464d90e789436c8a2f
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-28 23:35:07 +0300

    Add SQL query definitions and enhance SqlRepository methods for data retrieval

---
+++ src/server/data/index.ts
@@ -0,0 +1,7 @@
+import { Repository } from "./repository";
+import { SqlRepository } from "./sql_repository";
+
+
+const repository: Repository = new SqlRepository
+
+export default repository
---
+++ src/server/data/sql_queries.ts
@@ -0,0 +1,11 @@
+const baseSql = `
+    SELECT Results.*, name, age, years, nextage FROM Results
+    INNER JOIN People ON personId = People.id
+    INNER JOIN Calculations ON calculationId = Calcuations.id
+`
+
+const endSql = `ORDER BY id DESC LIMIT $limit`
+
+export const queryAllSql = `${baseSql} ${endSql}`
+
+export const queryByNameSql = `${baseSql} WHERE name = $name ${endSql}`
