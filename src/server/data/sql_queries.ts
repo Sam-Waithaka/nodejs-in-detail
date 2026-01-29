938d2e4404e4c2ff40750b5cd7560a9debcd3968
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-29 11:51:05 +0300

    Add SQL insert queries and implement TransactionHelper for database transactions

--- src/server/data/sql_queries.ts
+++ src/server/data/sql_queries.ts
@@ -9,3 +9,26 @@ const endSql = `ORDER BY id DESC LIMIT $limit`
 export const queryAllSql = `${baseSql} ${endSql}`
 
 export const queryByNameSql = `${baseSql} WHERE name = $name ${endSql}`
+
+export const insertPerson = `
+    INSERT INTO People (name)
+    SELECT $name
+    WHERE NOT EXISTS (SELECT name FROM People WHERE name = $name)
+`
+
+export const insertCalculation = `
+    INSERT INTO Calculations (age, years, nextage)
+    SELECT $age, $years, $nextage
+    WHERE NOT EXISTS
+        (SELECT age, years, nextage FROM Calcuations
+        WHERE age = $age AND years = $years AND nextage = $nextage)
+`
+
+export const insertResult = `
+    INSERT INTO Results (personId, calculationId)
+    SELECT People.id as personId, Calculations.id as calculationId from People
+    CROSS JOIN Calculations
+        WHERE People.name = $name
+        AND Calculations.years = $years
+        AND Calculations.nextage = $nextage
+`
