526f69796f5285f651e15f87d4ad0ac794d7586f
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-29 09:18:19 +0300

    Fix typo in SQL join condition and enhance form handling with repository data retrieval

--- src/server/data/sql_queries.ts
+++ src/server/data/sql_queries.ts
@@ -1,7 +1,7 @@
 const baseSql = `
     SELECT Results.*, name, age, years, nextage FROM Results
     INNER JOIN People ON personId = People.id
-    INNER JOIN Calculations ON calculationId = Calcuations.id
+    INNER JOIN Calculations ON calculationId = Calculations.id
 `
 
 const endSql = `ORDER BY id DESC LIMIT $limit`
--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,16 +1,24 @@
 import express, { Express } from "express";
+import repository from "./data";
+
+
+const rowLimit = 10
+
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({extended: true}))
 }
 export const registerFormRoutes = (app: Express) => {
-    app.get("/form", (req, resp) => {
-        resp.render("age");
+    app.get("/form", async (req, resp) => {
+        resp.render("age", {
+            history: await repository.getAllResults(rowLimit)
+        });
     });
-    app.post("/form", (req, resp) => {
+    app.post("/form", async (req, resp) => {
         const nextage = Number.parseInt(req.body.age)
             + Number.parseInt(req.body.years);
         const context = {
-            ...req.body, nextage
+            ...req.body, nextage,
+            history: await repository.getResultsByName(req.body.name, rowLimit)
         };
         resp.render("age", context);  
     });
