080e7413f18b8930125f0b55291252423ca4ce8b
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-30 12:18:07 +0300

    Seting and reading cookies

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import repository from "./data";
+import { getJsonCookie, setJsonCookie } from "./cookies";
 
 
 const rowLimit = 10
@@ -10,17 +11,27 @@ export const registerFormMiddleware = (app: Express) => {
 export const registerFormRoutes = (app: Express) => {
     app.get("/form", async (req, resp) => {
         resp.render("age", {
-            history: await repository.getAllResults(rowLimit)
+            history: await repository.getAllResults(rowLimit),
+            personalHistory: getJsonCookie(req, 'personalHistory')
         });
     });
+
+
     app.post("/form", async (req, resp) => {
         const nextage = Number.parseInt(req.body.age)
             + Number.parseInt(req.body.years);
-
-        await repository.saveResult({...req.body, nextage})
+        await repository.saveResult({...req.body, nextage });
+        let pHistory = [{
+            name: req.body.name, age: req.body.age,
+            years: req.body.years, nextage},
+            ...(getJsonCookie(req, "personalHistory") || [])].splice(0, 5);
+       
+        setJsonCookie(resp, "personalHistory", pHistory);
+       
         const context = {
             ...req.body, nextage,
-            history: await repository.getResultsByName(req.body.name, rowLimit)
+            history: await repository.getAllResults(rowLimit),
+            personalHistory: pHistory
         };
         resp.render("age", context);  
     });
