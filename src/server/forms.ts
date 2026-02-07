ee7c34e0038c0abf1166122c38a7ef730a3947c5
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-07 13:25:48 +0300

    Implement session management in forms module and add session helper

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express";
 import repository from "./data";
 import { getJsonCookie, setJsonCookie } from "./cookies";
 import cookieMiddleware from "cookie-parser";
+import { customSessionMiddleware } from "./sessions/middleware";
+import { getSession } from "./sessions/session_helpers";
 
 
 const rowLimit = 10
@@ -9,12 +11,13 @@ const rowLimit = 10
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({extended: true}))
     app.use(cookieMiddleware('mysecret'))
+    app.use(customSessionMiddleware())
 }
 export const registerFormRoutes = (app: Express) => {
     app.get("/form", async (req, resp) => {
         resp.render("age", {
             history: await repository.getAllResults(rowLimit),
-            personalHistory: getJsonCookie(req, 'personalHistory')
+            personalHistory: getSession(req).data.personalHistory
         });
     });
 
@@ -23,17 +26,25 @@ export const registerFormRoutes = (app: Express) => {
         const nextage = Number.parseInt(req.body.age)
             + Number.parseInt(req.body.years);
         await repository.saveResult({...req.body, nextage });
-        let pHistory = [{
-            name: req.body.name, age: req.body.age,
-            years: req.body.years, nextage},
-            ...(getJsonCookie(req, "personalHistory") || [])].splice(0, 5);
+        // let pHistory = [{
+        //     name: req.body.name, age: req.body.age,
+        //     years: req.body.years, nextage},
+        //     ...(getJsonCookie(req, "personalHistory") || [])].splice(0, 5);
        
-        setJsonCookie(resp, "personalHistory", pHistory);
+        // setJsonCookie(resp, "personalHistory", pHistory);
+
+        req.session.data.personalHistory = [{
+            name: req.body.name, 
+            age: req.body.age,
+            years: req.body.years, 
+            nextage
+        }, 
+        ...(req.session.data.personalHistory || [])].splice(0, 5)
        
         const context = {
             ...req.body, nextage,
             history: await repository.getAllResults(rowLimit),
-            personalHistory: pHistory
+            personalHistory: req.session.data.personalHistory
         };
         resp.render("age", context);  
     });
