41ebf986e20af586ae331352ad50aba782c3c3c8
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-07 14:09:49 +0300

    Implement OrmRepository for session management with SQLite support and define SessionModel

--- src/server/sessions/middleware.ts
+++ src/server/sessions/middleware.ts
@@ -1,14 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { SessionRepository, Session } from "./repository";
-import { MemoryRepository } from "./memory_repository";
+// import { MemoryRepository } from "./memory_repository";
 import { setCookie, getCookie } from "../cookies";
+import { OrmRepository } from "./orm_reository";
 
 
 const session_cookie_name = "custom_session";
 const expiry_seconds = 300;
 const getExpiryDate = () => new Date(Date.now() + (expiry_seconds * 1_000));
 export const customSessionMiddleware = () => {
-    const repo: SessionRepository = new MemoryRepository();
+    // const repo: SessionRepository = new MemoryRepository();
+    const repo: SessionRepository = new OrmRepository();
      return async (req: Request, resp: Response, next: NextFunction) => {
        
         const id = getCookie(req, session_cookie_name);
