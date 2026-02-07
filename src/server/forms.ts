bf262b210c7072c61b8f7e061d9845ea8dd522b8
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-07 14:38:21 +0300

    Add session management with Sequelize and SQLite support

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -2,8 +2,8 @@ import express, { Express } from "express";
 import repository from "./data";
 import { getJsonCookie, setJsonCookie } from "./cookies";
 import cookieMiddleware from "cookie-parser";
-import { customSessionMiddleware } from "./sessions/middleware";
-import { getSession } from "./sessions/session_helpers";
+import { customSessionMiddleware } from "./sessions/middleware"
+import { getSession, sessionMiddleware } from "./sessions/session_helpers";
 
 
 const rowLimit = 10
@@ -11,13 +11,14 @@ const rowLimit = 10
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({extended: true}))
     app.use(cookieMiddleware('mysecret'))
-    app.use(customSessionMiddleware())
+    // app.use(customSessionMiddleware())
+    app.use(sessionMiddleware())
 }
 export const registerFormRoutes = (app: Express) => {
     app.get("/form", async (req, resp) => {
         resp.render("age", {
             history: await repository.getAllResults(rowLimit),
-            personalHistory: getSession(req).data.personalHistory
+            personalHistory: getSession(req).personalHistory
         });
     });
 
@@ -33,18 +34,15 @@ export const registerFormRoutes = (app: Express) => {
        
         // setJsonCookie(resp, "personalHistory", pHistory);
 
-        req.session.data.personalHistory = [{
-            name: req.body.name, 
-            age: req.body.age,
-            years: req.body.years, 
-            nextage
-        }, 
-        ...(req.session.data.personalHistory || [])].splice(0, 5)
+        req.session.personalHistory = [{
+            id: 0, name: req.body.name, age: req.body.age,
+            years: req.body.years, nextage},
+            ...(req.session.personalHistory || [])].splice(0, 5);
        
         const context = {
             ...req.body, nextage,
             history: await repository.getAllResults(rowLimit),
-            personalHistory: req.session.data.personalHistory
+            personalHistory: req.session.personalHistory
         };
         resp.render("age", context);  
     });
