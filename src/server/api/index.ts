ac64726908c188e5306579872fec6d0d5e7c7dc7
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-13 12:22:28 +0300

    validation using feathers js package removing the need for custom validation

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -4,6 +4,27 @@ import { createAdapter } from "./http_adapter";
 import { ResultWebService } from "./results_api";
 import { Validator } from "./validation_adapter";
 import { ResultWebServiceValidation } from "./results_api_validation";
+import { FeathersWrapper } from "./feathers_adaper";
+import feathersExpress, {rest} from "@feathersjs/express";
+import { feathers } from "@feathersjs/feathers";
+import { ValidationError } from "./validation_types";
+
+
 export const createApi = (app: Express) => {
-    createAdapter(app, new Validator(new ResultWebService(), ResultWebServiceValidation), '/api/results')
+    // createAdapter(app, new Validator(new ResultWebService(), ResultWebServiceValidation), '/api/results')
+    const feathersApp = feathersExpress(feathers(), app).configure(rest())
+    const service = new Validator(new ResultWebService(), ResultWebServiceValidation)
+
+    feathersApp.use('/api/results', new FeathersWrapper(service))
+
+    feathersApp.hooks({
+        error: {
+            all: [(ctx) =>{
+                if (ctx.error instanceof ValidationError){
+                    ctx.http = {status: 400}
+                    ctx.error = undefined
+                }
+            }]
+        }
+    })
 }
