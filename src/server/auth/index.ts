9adce9e31a41f9b2066a9018f8c3fb0f4b804966
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-02-20 11:05:09 +0300

    Enhance authentication flow with user session management and sign-out functionality

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -5,12 +5,40 @@ import { OrmAuthStore } from "./orm_authstore";
 
 const store : AuthStore = new OrmAuthStore()
 
+type User = {username: string}
+declare module 'express-session'{
+    interface SessionData {username: string}
+}
+
+declare global{
+    module Express {
+        interface Request {user: User, authenticated: boolean}
+    }
+}
+
+
 export const createAuth = (app: Express)=>{
+
+    app.use((req, res, next)=>{
+        const username = req.session.username
+
+        if(username){
+            req.authenticated = true
+            req.user = {username}
+        } else {
+            req.authenticated = false
+        }
+        res.locals.user = req.user
+        res.locals.authenticated = req.authenticated
+        next()
+    })
+
     app.get('/signin', (req, res)=>{
         const data = {
             username: req.query['username'],
             password: req.query['password'],
-            failed: req.query['failed'] ? true : false
+            failed: req.query['failed'] ? true : false,
+            signinpage: true
         }
         res.render('signin', data)
     })
@@ -20,9 +48,16 @@ export const createAuth = (app: Express)=>{
         const password = req.body.password
         const valid = await store.validateCredentials(username, password)
         if (valid){
+            req.session.username = username
             res.redirect('/')
         } else {
             res.redirect(`/signin?username=${username}&password=${password}&failed=1`)
         }
+    })
+
+    app.post('/signout', async (req, res)=>{
+        req.session.destroy(()=>{
+            res.redirect('/')
+        })
     })
 }
